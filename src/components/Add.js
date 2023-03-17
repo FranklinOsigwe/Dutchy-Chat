de2b63c6fccb7993ddbcbcbc7d1ff48a9ceb0f89
Franklin Osigwe <franklindutch1@gmail.com>
2023-03-17 23:57:49 +0100

    Post feature almost done

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,9 +1,76 @@
-import React from 'react'
+import { Fab, Tooltip, styled, Modal, Typography, Avatar, TextField } from "@mui/material";
+import React, { useState } from "react";
+import { Add as AddIcon, EmojiEmotions, VideoCameraBack ,Image,PersonAdd} from "@mui/icons-material";
+import Box from "@mui/material/Box";
+import { Stack } from "@mui/system";
+
+const StyledModal = styled(Modal)({
+  display: "flex",
+  alignItems: "center",
+    justifyContent: "center",
+  marginBottom: '20px '
+});
+
+const UserBox = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+});
 
 function Add() {
+  const [open, setOpen] = useState(false);
   return (
-    <div>Add</div>
-  )
+    <>
+      <Tooltip
+        onClick={(e) => setOpen(true)}
+        title="Delete"
+        sx={{
+          position: "fixed",
+          bottom: 20,
+          left: { xs: "calc(50% - 25px)", md: 30 },
+        }}
+      >
+        <Fab color="primary" aria-label="add">
+          <AddIcon />
+        </Fab>
+      </Tooltip>
+
+      <StyledModal
+        open={open}
+        onClick={(e) => setOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box width={400} height={280} bgcolor="white" p={3} borderRadius={5}>
+        <Typography variant="h6" color="gray" textAlign="center">
+          {" "}
+          Create Post
+                  </Typography>
+                  
+                  <UserBox>
+                      <Avatar src="https://www.nj.com/resizer/zovGSasCaR41h_yUGYHXbVTQW2A=/1280x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/SJGKVE5UNVESVCW7BBOHKQCZVE.jpg" sx={{ width: 30, height: 30 }} />
+                      
+                      <Typography fontWeight={500} pl={2} >Franklin D</Typography>
+                  </UserBox>
+                  <TextField sx={{width:'100%'}}
+          id="standard-multiline-static"
+        //   label="Multiline"
+          multiline
+          rows={3}
+          placeholder="What's on your mind"
+          variant="standard"
+        />
+                  <Stack direction="row" gap={1} mt={3} mb={3}>
+                      <EmojiEmotions color="primary"/>
+                      <Image color='secondary'/>
+                      <VideoCameraBack color="success" />
+                      <PersonAdd color="error" />
+              </Stack>
+              </Box>
+              
+        
+      </StyledModal>
+    </>
+  );
 }
 
-export default Add
+export default Add;
